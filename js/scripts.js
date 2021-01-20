0c9c0397fe0a108ebc66f8a96ec32cdf6086cc94
Morgan Bradford <morganjbradford95@gmail.com>
2021-01-19 22:02:01 -0600

    add submit(), define let userResponses, tally. Create tentative for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-  
+  $("form#stress-test").submit(function(event) {
+    event.preventDefault();
+    let userResponses = []
+    let tally = [];
+    for (let i = 0; i < userResponses.length; i += 2) {
+      for (let j = 1; i < userResponses.length; i += 2) {
+      tally.push(userResponses[i] + userResponses[j]);
+      }
+    }
+    
+  });
 });

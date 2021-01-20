37d765a583cc2455912bed7ee17728f03b7aab24
Morgan Bradford <morganjbradford95@gmail.com>
2021-01-19 22:02:33 -0600

    add input receivers

--- js/scripts.js
+++ js/scripts.js
@@ -9,5 +9,28 @@ $(document).ready(function() {
       }
     }
     
+    $("#test-responses").show();
+    $("input:checkbox[name=sign]:checked").each(function() {
+      const stressSigns = parseInt($(this).val());
+      userResponses.push(stressSigns);
+    });
+    $("input:checkbox[name=symptom]:checked").each(function() {
+      const stressSymptoms = parseInt($(this).val());
+      userResponses.push(stressSymptoms);
+    });
+    $("input:checkbox[name=relief]:checked").each(function() {
+      const reliefActivity = parseInt($(this).val());
+      userResponses.push(reliefActivity);
+    });
+    
+    $('#stress-test').hide();
+    
+    //if () {
+    //  $("#mild-stress").show();
+    //} else if () {
+    //  $("#significant-stress").show();
+    //} else {
+    //  $("#what-stress").show();
+   // }
   });
 });
